a7d88f7632b92f19ea061927bd5760590a24d941
Reynir Björnsson <reynir@reynir.dk>
2025-10-01 13:04:16 +0200

    Fix test

--- test/main.ml
+++ test/main.ml
@@ -1,9 +1,9 @@
 let app () =
   let open Lwt.Syntax in
   Printf.printf "Hello!\n%!";
-  let* () = OS.Time.sleep_ns 1_000_000_000L in
+  let* () = Mirage_sleep.ns 1_000_000_000L in
   Printf.printf "After 1s\n%!";
-  let+ () = OS.Time.sleep_ns 1_000_000_000L in
+  let+ () = Mirage_sleep.ns 1_000_000_000L in
   Printf.printf "After 2s\n%!"
 
-let () = OS.Main.run (app ())
+let () = Solo5_os.Main.run (app ())
